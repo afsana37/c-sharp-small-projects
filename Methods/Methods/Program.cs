d87067cd69a2f1b9fe0ea8a86e4b908cbaeb7352
Afsana <afsana.abida@stud.uni-bamberg.de>
2021-11-17 17:13:03 +0100

    Method Done

---
+++ Methods/Methods/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Array // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            SayHi("Mike Hannigan", 33);
+            SayHi("Paul Rudd", 52);
+            SayHi("Ant Man", 40);
+            Console.ReadLine();
+
+
+        }
+
+        static void SayHi(string name, int age)
+        {
+            Console.WriteLine("Hello " + name + " your ages is " + age);
+        }
+    }
+}
