eff90d800c037f46cecdabeb2f717a6acfd040e0
Afsana <afsana.abida@stud.uni-bamberg.de>
2021-11-17 17:25:20 +0100

    Return Statement done

---
+++ Return/Return Statement/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Return // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            int cubedNumber = cube(5);
+            Console.WriteLine(cubedNumber);
+            Console.ReadLine();
+
+
+        }
+
+        static int cube(int num)
+        {
+            int result = num * num * num;
+            return result;
+        }
+    }
+}
