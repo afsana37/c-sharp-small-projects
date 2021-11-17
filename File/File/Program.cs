82bfc42082dd2ac63bfa3861b23f211537f46a42
Afsana <afsana.abida@stud.uni-bamberg.de>
2021-11-17 18:59:57 +0100

    File Read and Write

---
+++ File/File/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.IO; // include the System.IO namespace
+
+string writeText = "This is a test";
+
+//Create a file and write the content of writeText to it
+
+File.WriteAllText("filename.txt", writeText);
+
+// Read the contents of the file
+string readText = File.ReadAllText("filename.txt");
+
+// Output the content
+Console.WriteLine(readText);
