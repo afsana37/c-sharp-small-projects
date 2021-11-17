369b5989e3aaba09b64214ecacf51d7319370e0c
Afsana <afsana.abida@stud.uni-bamberg.de>
2021-11-17 18:41:34 +0100

    Switch Case

---
+++ SwitchStatement/SwitchStatement/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SwitchStatement // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(GetDay(5));
+            Console.ReadLine();
+
+
+        }
+
+        static string GetDay(int dayNum)
+        {
+            string dayName;
+
+            switch(dayNum)
+            {
+                case 0:
+                    dayName = "Sunday";
+                    break;
+                case 1:
+                    dayName = "Monday";
+                    break;
+                case 2:
+                    dayName = "Tuesday";
+                    break;
+                case 3:
+                    dayName = "Wednesday";
+                    break;
+
+                case 4:
+                    dayName = "Thursday";
+                    break;
+                case 5:
+                    dayName = "Friday";
+                    break;
+                case 6:
+                    dayName = "Saturday";
+                    break;
+                default:
+                    dayName = "Invalid day Number";
+                    break;
+
+            }
+            return dayName;
+        }
+    }
+}
